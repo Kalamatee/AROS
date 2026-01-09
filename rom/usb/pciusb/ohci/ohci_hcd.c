e994944a20f408cc89e063beec8b87af9b70d0c7
Nick Andrews <kalamatee@gmail.com>
2026-01-09 23:36:50 +0000

    Refactor isoch PTD chipset fields

--- rom/usb/pciusb/ohci/ohci_hcd.c
+++ rom/usb/pciusb/ohci/ohci_hcd.c
@@ -381,7 +381,7 @@ static void ohciHandleFinishedTDs(struct PCIController *hc)
                     struct PTDNode *scan = rtn->rtn_PTDs[idx];
                     if (scan && scan->ptd_Descriptor == oitd) {
                         ptd = scan;
-                        bufreq = &scan->ptd_BufferReq;
+                        bufreq = &ohciPTDPrivate(scan)->ptd_BufferReq;
                         break;
                     }
                 }
@@ -450,11 +450,14 @@ static void ohciHandleFinishedTDs(struct PCIController *hc)
             if (bufreq)
                 bufreq->ubr_Length = ioreq->iouh_Actual;
 
-            if (ptd && ptd->ptd_BounceBuffer &&
-                    ptd->ptd_BounceBuffer != ptd->ptd_BufferReq.ubr_Buffer) {
-                usbReleaseBuffer(ptd->ptd_BounceBuffer, ptd->ptd_BufferReq.ubr_Buffer,
-                                 ptd->ptd_BufferReq.ubr_Length, ioreq->iouh_Dir);
-                ptd->ptd_BounceBuffer = NULL;
+            if (ptd) {
+                struct OhciPTDPrivate *ptdpriv = ohciPTDPrivate(ptd);
+                if (ptdpriv->ptd_BounceBuffer &&
+                        ptdpriv->ptd_BounceBuffer != ptdpriv->ptd_BufferReq.ubr_Buffer) {
+                    usbReleaseBuffer(ptdpriv->ptd_BounceBuffer, ptdpriv->ptd_BufferReq.ubr_Buffer,
+                                     ptdpriv->ptd_BufferReq.ubr_Length, ioreq->iouh_Dir);
+                    ptdpriv->ptd_BounceBuffer = NULL;
+                }
             }
 
             if (rtiso && rtn && rtn->rtn_RTIso) {
@@ -1440,6 +1443,11 @@ static AROS_INTH1(ohciIntCode, struct PCIController *, hc)
 
 #undef base
 #define base (hc->hc_Device)
+
+static inline struct OhciPTDPrivate *ohciPTDPrivate(struct PTDNode *ptd)
+{
+    return (struct OhciPTDPrivate *)ptd->ptd_Chipset;
+}
 
 /*
  * CHECKME: This routine is implemented according to the OHCI specification, however poorly tested.
